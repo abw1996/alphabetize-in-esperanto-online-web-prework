55b2c49a5cb6979d4d0237af5585e058db314eae
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-10 03:14:14 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,5 +3,5 @@ require 'pry'
 ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-    arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+    arr.sort_by { |a| a.split("").collect{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
