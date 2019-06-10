79a1458631fa06bf633fa89ebab0b81b38ae6d9c
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-10 03:01:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,8 @@
 require 'pry'
 def alphabetize(arr)
   alpha = ("abcĉdefgĝhĥijĵklmnoprsŝtuŭvz").split("")
-  alphabetized = arr.sort_by do |word|
+  alphabetized = arr.sort_by do |word1, word2|
     alpha.index(word[0])
+    
   end
 end
