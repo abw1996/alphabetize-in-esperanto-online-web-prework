00f6ecd5e657a982e37319f59f6c142ce14ed943
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-10 02:45:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alpha = ("abcĉdefgĝhĥijĵklmnoprsŝtuŭvz").split("")
+  alphabetized = arr.sort_by do |word|
+    index_counter = 0
+    binding.pry
+    while alpha.index(word1[index_counter]) == alpha.index(word2[index_counter])
+      index_counter += 1
+    end 
+    binding.pry
+    if alpha.index(word1[index_counter]) > alpha.index(word2[index_counter])
+      
+    end
+  end
 end
